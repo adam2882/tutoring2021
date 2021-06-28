0c3421fce6c19b23acbf900be8fbbb36f110ec93
rsowers2019 <rosie.sowers@gmail.com>
2021-06-28 13:41:12 -0700

    Naming changes to water/models.py
    Switched to gis import to allow for later geo data fields
    Renamed fields to match typical naming conventions
    Added missing depth field
    Changed max length of station type to 20

--- adam_tutoring/water/models.py
+++ adam_tutoring/water/models.py
@@ -1,19 +1,20 @@
-from django.db import models
+from django.contrib.gis.db import models
 
 class Station(models.Model):
-	station_id = models.IntegerField(primary_key=True)
-	station_name = models.CharField(max_length=40)
-	station_type = models.CharField(max_length=11)
+	id = models.IntegerField(primary_key=True)
+	name = models.CharField(max_length=40)
+	station_type = models.CharField(max_length=20)
 	latitude = models.FloatField()
 	longitude = models.FloatField()
 	county = models.CharField(max_length=30)
 
 class LabResult(models.Model): 
-	station_id = models.ForeignKey(Station, on_delete=models.CASCADE)
+	station = models.ForeignKey(Station, on_delete=models.CASCADE)
 	status = models.CharField(max_length=40)
 	sample_code = models.CharField(max_length=20)
-	sample_date = models.DateField()
-	sample_depth_units = models.CharField(max_length=15)
+	date = models.DateField()
+	depth = models.FloatField()
+	depth_units = models.CharField(max_length=15)
 	parameter = models.CharField(max_length=40)
 	result = models.FloatField()
 	reporting_limit = models.FloatField()
