d415c24e83ee1419db1c915409fa43caa2808bbc
adam2882 <85712653+adam2882@users.noreply.github.com>
2021-06-30 16:25:21 -0700

    create views and create urls
    created views with an html table to display data, and created urls to help display the views

--- adam_tutoring/water/views.py
+++ adam_tutoring/water/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Station
 
-# Create your views here.
+def index(request):
+    station_results = Station.objects.all()
+    station_table = '<table><tr><th>Name</th><th>Latitude</th><th>Longitude</th><th>County</th></tr>'
+    for station in station_results:
+        station_table += '<tr><td>'
+        station_table += '</td><td>'.join([station.name, str(station.latitude), str(station.longitude), station.county])
+        station_table += '</td></tr>'
+    station_table += '</table>'
+    return HttpResponse(station_table)
