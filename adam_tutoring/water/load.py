544368f282b1e79467969fa0b720e8e012dee7c1
rsowers2019 <rosie.sowers@gmail.com>
2021-07-05 15:30:12 -0700

    Added code to load lab results database
    Also updated gitignore to get rid of unnecessary cache files

--- adam_tutoring/water/load.py
+++ adam_tutoring/water/load.py
@@ -18,3 +18,48 @@ def load_stations():
 					longitude=row['LONGITUDE'], 
 					county=row['COUNTY_NAME'])
 				station.save()
+
+
+def float_helper(input_string):
+	if input_string == '':
+		return
+	else:
+		return(float(input_string))
+
+def convert_date_helper(datetime_string):
+	# Changes datetime of format 05/03/1967 09:00 and returns date in YYYY-MM-DD format
+	date = datetime_string.split()[0]
+	date_parts = date.split('/')
+	return(date_parts[2] + '-' + date_parts[0] + '-' + date_parts[1])
+
+
+def load_lab_results():
+	with open('water/data/lab-results.csv', newline='') as csvfile:
+		reader = csv.DictReader(csvfile)
+		for counter, row in enumerate(reader):
+			if counter % 10000 == 0:
+				print(counter)
+			try:
+				station = Station.objects.get(pk=int(row['STATION_ID']))
+			except Station.DoesNotExist:
+				print(row)
+				print('Station not found in database')
+			else:
+				if row['RESULT'] != '':
+					# Only add entries that actually have a result
+					# Depth allowed to be missing, all other fields are required
+					result = LabResult(
+						station=station,
+						status=row['STATUS'],
+						sample_code=row['SAMPLE_CODE'],
+						date=convert_date_helper(row['SAMPLE_DATE']),
+						depth=float_helper(row['SAMPLE_DEPTH']),
+						depth_units=row['SAMPLE_DEPTH_UNITS'],
+						parameter=row['PARAMETER'],
+						result=row['RESULT'],
+						reporting_limit=float_helper(row['REPORTING_LIMIT']),
+						units=row['UNITS'],
+						method_name=row['METHOD_NAME']
+						)
+					result.save()
+
